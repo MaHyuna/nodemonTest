1c6853615e62129974367d5f218a04cf19e9f0c6
maro <35321446+MaHyuna@users.noreply.github.com>
2020-01-08 10:55:48 +0900

    nodemon test
    first commit

---
+++ nodeJsEx04.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// nodemon 설치는 npm i -g nodemon
+// 실행은 nodemon .\파일명.js 하면 됨 >> 현재파일 nodemon .\nodeJsEx04.js
+// 저장 시 계속 출력됨
+
+const var1 = 'test // nodemon은 실시간으로 수정사항을 보여준다...123'
+
+console.log(var1)
